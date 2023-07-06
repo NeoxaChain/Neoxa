92df47f112777a42ee847712b28a0835bbe5a071
Andy <108697117+AndyNeoxa@users.noreply.github.com>
2023-07-06 16:31:27 +0100

    Update utilitydialog.cpp

--- src/qt/utilitydialog.cpp
+++ src/qt/utilitydialog.cpp
@@ -161,8 +161,7 @@ your funds will already be mixed. No additional waiting is required.</li> \
 <b>IMPORTANT:</b> Your wallet only contains 1000 of these \"change addresses.\" Every time a mixing event happens, up to 9 of your addresses are used up. \
 This means those 1000 addresses last for about 100 mixing events. When 900 of them are used, your wallet must create more addresses. \
 It can only do this, however, if you have automatic backups enabled.<br> \
-Consequently, users who have backups disabled will also have PrivateSend disabled. <hr>\
-For more information, see the <a href=\"https://docs.neoxa.org/en/stable/wallets/neoxacore/privatesend-instantsend.html\">PrivateSend documentation</a>."
+Consequently, users who have backups disabled will also have PrivateSend disabled. <hr>\"
         ));
         ui->aboutMessage->setWordWrap(true);
         ui->helpMessage->setVisible(false);
