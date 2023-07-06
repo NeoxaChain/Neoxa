102ef32f60ce330a34588a190eec6001b402f7d6
Andy <108697117+AndyNeoxa@users.noreply.github.com>
2023-07-06 16:33:20 +0100

    Update neoxad.cpp

--- src/neoxad.cpp
+++ src/neoxad.cpp
@@ -33,7 +33,7 @@
  *
  * \section intro_sec Introduction
  *
- * This is the developer documentation of the reference client for an experimental new digital currency called Neoxa (https://www.neoxa.org/),
+ * This is the developer documentation of the reference client for an experimental new digital currency called Neoxa (https://www.neoxa.net/),
  * which enables instant payments to anyone, anywhere in the world. Neoxa uses peer-to-peer technology to operate
  * with no central authority: managing transactions and issuing money are carried out collectively by the network.
  *
